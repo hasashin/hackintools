52ef728532b210f27f9c25fc578d3e181a9fdad5
hasashin <dominik.hazak@gmail.com>
2020-11-04 23:59:29 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+Hackintools plugin
+===
+
+This is plugin for Oh My Zsh framework for hackintosh macOS users.
+
+To install this plugin clone this repo into `$ZSH/custom/plugins` folder, then enable it in `~/.zhrc`:
+
+```
+plugins=(... hackintools)
+```
+## Commands
+
+| Command    | Description                                             |
+|:-----------|:--------------------------------------------------------|
+|`openvs`    |Open file or folder in Visual Studio Code                |
+|`mountefi`  |List and mount EFI partitions found on the system        |
+|`umountefi` |Umount EFI partitions previously mounted via `mountefi`  |
+
+---
+
+## License
+
+Copyright 2020 [hasashin](https://github.com/hasashin)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ hackintools.plugin.zsh
@@ -0,0 +1,64 @@
+# Open given file/folder in Visual Studio Code
+alias openvs='open -a Visual\ Studio\ Code $* 2> /dev/null'
+
+# Mount EFI partition
+function mountefi() {
+	local mountDisk
+	local guid
+	local tempFile='/tmp/mountefi'
+	if [[ $# > 0 ]]; then
+		key="$1"
+		if [[ "$key" == "default" ]]; then 
+			if [[ -v MOUNTEFI_DEFAULT_VOLUME_GUID ]]; then
+				guid=$MOUNTEFI_DEFAULT_VOLUME_GUID
+			else
+				echo "There's no default volume guid set!" 
+				echo "Please export MOUNTEFI_DEFAULT_VOLUME_GUID variable to use 'default' option."
+			fi
+		elif [[ "$key" == "list" ]]; then
+			LISTONLY=YES
+		elif [[ "$key" == "help" || "$key" == "--help" ]]; then
+			echo "Usage: efimount [option]"
+			echo "Available options:"
+			echo "\tdefault\t\tmounts default efi volume defined in MOUNTEFI_DEFAULT_VOLUME_GUID variable"
+			echo "\tlist\t\tlists available efi volumes"
+			echo "\t/dev/diskx\tmounts given partition"
+			echo "\thelp\t\tdisplays this message"
+			return 0
+		else
+			echo "Unknown option: \"$key\""
+			echo "run: '$0 help' to show available usage."
+			return -1
+		fi
+	fi
+	diskutil list -plist physical > $tempFile 2> /dev/null
+	if [[ -z "$guid" ]]; then
+
+		#print usable information about EFI partitions
+
+		if [[ -n "$LISTONLY" ]]; then
+			return 0
+		fi
+		rm -f $tempFile 2> /dev/null
+		return -1
+		#chose disk	
+	fi
+	mountDisk="/dev/$(cat $tempFile | grep -B 11 -h $guid | grep -ho 'disk.s.')"
+	rm -f $tempFile 2> /dev/null
+	sudo mkdir "/Volumes/EFI-$guid"
+	sudo mount -t msdos $mountDisk "/Volumes/EFI-$guid"
+	echo $mountDisk
+	[[ -e "/tmp/mountedEfi" ]] && echo "$mountDisk " >> "/tmp/mountedEfi" || echo "$mountDisk " > "/tmp/mountedEfi"
+	return 0
+}
+
+function umountefi() {
+	local mountedEfis
+	if [[ -e "/tmp/mountedEfi" ]]; then 
+		mountedEfis="$(cat /tmp/mountedEfi)"
+		for i in $mountedEfis; do 
+			diskutil umount force $i > /dev/null
+		done
+		rm -f "/tmp/mountedEfi" 2> /dev/null
+	fi
+}
